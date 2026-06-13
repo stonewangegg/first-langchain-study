139215592aa1c199e8b891ae226c68f6cfaf6851
stonewangegg <hz.stonew@hotmail.com>
2026-06-13 21:55:03 +0800

    update the local mode and local base url

--- third_agents_demo/analyzer_agent_demo.py
+++ third_agents_demo/analyzer_agent_demo.py
@@ -84,8 +84,8 @@ FILE_DIR = os.environ.get("FILE_DIR", "./tmp")
 FILE_ROOT_DIR = str(Path(CURRENT_WORKING_DIR) / Path(FILE_DIR))
 
 # llm info
-LOCAL_MODEL="qwen3.6:27b"
-LOCAL_BASEURL="http://172.30.0.1:11434"
+LOCAL_MODEL="Qwen/Qwen3.6-35B-A3B-FP8"
+LOCAL_BASEURL="http://192.168.8.50:8000/v1"
 ONLINE_MODEL="minimax-m3:cloud"
 ONLINE_BASEURL="http://172.30.0.1:11434"
 
@@ -154,13 +154,13 @@ toolCallLimitMiddleware = cast(AgentMiddleware, ToolCallLimitMiddleware(tool_nam
 
 # inital the model object of Ollama provider
 model_ollama = ChatOllama(
-            model=ONLINE_MODEL,
+            model=LOCAL_MODEL,
             validate_model_on_init=True,
             # num_thread=16,
             verbose=True,                       # Print additional LangChain logs.Useful for debugging: prompts, tool calls, intermediate chains
             reasoning=False,
             temperature=0.5,
-            base_url=ONLINE_BASEURL,
+            base_url=LOCAL_BASEURL,
             repeat_penalty=1.05,
             num_ctx=int(MAX_COMPLETION_TOKENS),
             disable_streaming="tool_calling"
